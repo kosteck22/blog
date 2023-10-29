b27d1e4854e669a106006cef32a12dab4a16aac0
Kamil Ostafil <kosteck22@wp.pl>
2023-10-29 13:32:50 +0100

    added user entity

---
+++ src/main/java/com/example/blog/user/User.java
@@ -0,0 +1,35 @@
+package com.example.blog.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "phone", nullable = false)
+    private String phone;
+}
