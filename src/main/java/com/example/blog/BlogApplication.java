85f63e9e5ba92831706b30bb808221ab8971fca4
Kamil Ostafil <kosteck22@wp.pl>
2024-09-22 21:16:49 +0200

    test changes

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -10,5 +10,9 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("d");
 	}
 }
